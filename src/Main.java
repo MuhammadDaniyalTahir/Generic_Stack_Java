14a34b42b8ef185e996aea5262935e2d97ff180f
Muhammad Daniyal Tahir <muhammaddaniyaltahir969@gmail.com>
2023-10-04 21:09:13 +0500

    Generic stack completed.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         runGenericStack();
         runNonRepeating();
+        runMergeIntervals();
+
     }
     static void runGenericStack(){
-        GenericStack<Integer> st = new GenericStack<Integer>();
+        System.out.println("\n****************Running Generic Stack*************");
+        GenericStack st = new GenericStack();
+        FirstNonRepeatingStream obj1 = new FirstNonRepeatingStream();
+        FirstNonRepeatingStream obj2 = new FirstNonRepeatingStream();
+        FirstNonRepeatingStream obj3 = new FirstNonRepeatingStream();
         st.push(1);
         st.push(2);
+        st.push(obj2);
+        st.push(obj3);
+        st.push(obj1);
         st.push(3);
         st.push(4);
+
         System.out.println("Stack size(): " + st.size());
         try{
             System.out.println("Stack pop(): " + st.pop());
@@ -23,6 +32,7 @@ public class Main {
         System.out.println("Stack isEmpty(): " + st.isEmpty());
     }
     static void runNonRepeating(){
+        System.out.println("\n****************Non Repeating Stream*************");
         FirstNonRepeatingStream stream = new FirstNonRepeatingStream();
         stream.add('a');
         stream.add('b');
@@ -34,5 +44,40 @@ public class Main {
         stream.add('t');
         stream.add('f');
         System.out.println("First non-repeating element is: " + stream.getFirstNonRepeating());
+    }
+    static void runMergeIntervals(){
+        System.out.println("\n****************Merging Intervals*************");
+        int[] arr1 = {1,3};
+        int[] arr2 = {2,6};
+        int[] arr3 = {8,10};
+        int[] arr4 = {15,18};
+        int[] arr5 = {80,100};
+
+        List<int[]> list = new ArrayList<int[]>();
+        list.add(arr1);
+        list.add(arr2);
+        list.add(arr3);
+        list.add(arr4);
+        list.add(arr5);
+        System.out.println("List before merging");
+        displayList(list);
+        mergeIntervals(list);
+        System.out.println("List after merging");
+        displayList(list);
+    }
+    static void mergeIntervals(List<int[]> intervals){
+        for(int i = 0; i < intervals.size()-1; i++){
+            if(intervals.get(i)[1] > intervals.get(i+1)[0]){
+                intervals.add(i, new int[]{intervals.get(i)[0], intervals.get(i+1)[1]});
+                intervals.remove(i+1);
+                intervals.remove(i+1);
+                i--;
+            }
+        }
+    }
+    static void displayList(List<int[]> intervals){
+        for(int i = 0; i < intervals.size(); i++){
+            System.out.println("[" + intervals.get(i)[0] + "," + intervals.get(i)[1] + "]");
+        }
     }
 }
